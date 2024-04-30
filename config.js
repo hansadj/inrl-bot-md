228f915254a4f8b0ad34f3a4488edfa36f11fef6
hansadj <148874027+hansadj@users.noreply.github.com>
2024-04-30 20:49:50 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ if (existsSync('config.env')) require('dotenv').config({ path: './config.env' })
 process.env.NODE_OPTIONS = '--max_old_space_size=2560'//2.5
 const DB_URL =  process.env.DATABASE_URL || '';
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || '', //your ssid to run bot
+    SESSION_ID: process.env.SESSION_ID || 'inrl~08f24DzZb6b269b1f5fb4a6df80cc0daf8f1', //your ssid to run bot
     HEROKU: {
         API_KEY: process.env.HEROKU_API_KEY,
         APP_NAME: process.env.HEROKU_APP_NAME
